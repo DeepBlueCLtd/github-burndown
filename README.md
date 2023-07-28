8f94e747f1b5b12e1bb1fddd532efb3ffe3789c1
Ahmad Elang <sayaelang@gmail.com>
2023-07-28 13:36:06 +0700

    add code.js and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# github-burndown
-Generate burndown chart from GitHub issues, via Google Sheets
+# GitHub Burndown Chart Generator
+
+## Objective:
+The GitHub Burndown Chart Generator aims to create a burndown chart from GitHub issues using Google Sheets through the power of Apps Script.
+
+## How to Run:
+
+1. Create a new Google Sheets document.
+2. Create a sheet named "CONFIG," which will hold the configuration for the script.
+    - Only include information in the "CONFIG" sheet that you want people to see. Keep in mind that even if you hide the sheet, users with view access can still duplicate the sheet and potentially access the hidden data.
+    - For more information on hiding sheets in Google Sheets, you can refer to this support article: https://support.google.com/docs/answer/1218656?hl=en&co=GENIE.Platform%3DDesktop&oco=0#
+3. On the "CONFIG" sheet, place the following table in the top left corner (A1:B4). The order doesn't matter, but all fields are required:
+```
+PROJECT_NUMBER,3
+ORG_NAME,DeepBlueCLtd
+GRAPH_TITLE,RCO Burndown Chart
+SHEET_NAME,RCO
+```
+4. Add your GitHub token to the script properties:
+    - Follow this guide to obtain your GitHub token: [GitHub - Managing your personal access tokens](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#personal-access-tokens-classic). For this script, you only need the `project: read:project` access.
+        - This video provide step by step tutorial on generating github token [Youtube]https://www.youtube.com/watch?v=9lGcbQR4k4Y
+    - In Apps Script, go to Project Settings -> Edit Script Properties.
+    - Add a property named `GITHUB_TOKEN` and set its value as your GitHub token.
+5. Create another sheet as your target sheet (RCO by default), which can have any name, as long as it matches the value of the `SHEET_NAME` field from the CONFIG sheet.
+6. Set up the Apps Script:
+    - Go to Extensions -> Apps Script.
+    - Copy the script from `code.js` to `code.gs` in Apps Script.
+    - Set it to run the `renderResult` function.
+    - Try running the script, and if everything is working correctly, it should display something like this in your target sheet:
+```
+RCO Burndown Chart,,,,
+Request Date,Done,Done (Hour),Not Done,Not Done (Hour)
+2023-07-27 10:28,7,24,39,229
+```
+7. Set up Triggers. Press "Add Triggers."
+    - Set the event source to Time-Driven and choose the desired interval.
+    - You can choose any trigger that works, as long as the `Choose which function to run` field is set to: `renderResult`.
+8. From this Google Sheet, you can generate a burndown chart for your project.
+    <!-- TODO: Add this part -->
+    1. 
+9. Publish the image and add it to your GitHub page.
+
+## How to Fetch a GitHub API Token:
+
+## Process
+![Process Diagram](assets/diagram.png)
+
+## Lessons Learned
+
+### GitHub GraphQL API
+The GitHub GraphQL API can be quite challenging, especially when fetching custom fields like "Status" and "Hours." It requires some exploration to use effectively.
+
+One issue we encountered was fetching custom fields in GitHub v2 projects using `fieldValueByName(name: $field_name)`. However, this method proved problematic when attempting to fetch multiple custom fields simultaneously.
+
+The solution was to use GraphQL aliases: [Using Aliases in GraphQL](https://blog.logrocket.com/using-aliases-graphql/).
+
+```
+hours: fieldValueByName(name: "Hours") {
+... on ProjectV2ItemFieldSingleSelectValue {
+    value: name
+    }
+}
+status: fieldValueByName(name: "Status") {
+    ... on ProjectV2ItemFieldSingleSelectValue {
+        status: name
+    }
+}
+```
+
+This approach worked perfectly and also made processing the response easier.
+
+### Google Apps Script
+
+Setting up Apps Script was surprisingly straightforward. Although securing tokens remained challenging since users with edit access could see your GITHUB_TOKEN script properties. If you find a better way to handle this, feel free to message us. To prevent your GitHub token from being leaked, it's recommended to avoid sharing edit access with others.
+
+There are some minor differences compared to regular JavaScript, like using `UrlFetchApp` for API requests. But for simple scripts, your code should mostly work out of the box with Google Apps Script.
---
+++ code.js
@@ -0,0 +1,138 @@
+const PROJECT_NUMBER = 3;
+const ORG_NAME = "DeepBlueCLtd"
+const DONE = "Done";
+const GITHUB_TOKEN=PropertiesService.getScriptProperties().getProperty("GITHUB_TOKEN")
+const CONFIG_SHEET_NAME = "CONFIG";
+const PER_PAGE = 100;
+const HEADERS = ["Request Date", "Done", "Done (Hour)", "Not Done", "Not Done (Hour)"]
+
+function renderResult() {
+  let summary = {
+    countDone: 0,
+    countNotDone: 0,
+    sumHoursDone: 0,
+    sumHoursNotDone: 0,
+  };
+  const config = fetchConfig()
+  let variables = {
+    orgName: config.ORG_NAME,
+    projectNumber: config.PROJECT_NUMBER,
+    pages: PER_PAGE,
+    after: null
+  };
+  summary = sumProjectHoursByStatus(GITHUB_TOKEN, variables, summary);
+  let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(config.SHEET_NAME);
+  let row = getFirstEmptyRowByColumnArray(sheet);
+  row = setAndSkipHeader(config, sheet, row)
+  let range = sheet.getRange(row, 1, 1, 5);
+  const utcNow = Utilities.formatDate(new Date(), 'Etc/GMT', 'yyyy-MM-dd HH:mm')
+  range.setValues([[utcNow, summary.countDone, summary.sumHoursDone, summary.countNotDone, summary.sumHoursNotDone]]);
+}
+
+function fetchConfig() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(CONFIG_SHEET_NAME);
+  const configRange = sheet.getRange(1, 1, 5, 2);
+  const configList = configRange.getValues();
+  let configMap = {}
+  for (const element of configList) {
+    configMap[element[0]] = element[1]
+  }
+  return configMap;
+}
+
+function setAndSkipHeader(config, sheet, row) {
+  if (row === 1) {
+    let columHeader = sheet.getRange(1, 1, 1, 1);
+    columHeader.setValues([[config.GRAPH_TITLE]]);
+    columHeader = sheet.getRange(2, 1, 1, 5);
+    columHeader.setValues([HEADERS]);
+    return 3
+  }
+  if (row === 2) {
+    let columHeader = sheet.getRange(2, 1, 1, 5);
+    columHeader.setValues([HEADERS]);
+    return 3
+  }
+  return row
+}
+
+// From answer https://stackoverflow.com/a/9102463/1677912
+function getFirstEmptyRowByColumnArray(spr) {
+  let column = spr.getRange('A:A');
+  let values = column.getValues();
+  let ct = 0;
+  while (values[ct] && values[ct][0] != "") {
+    ct++;
+  }
+  return (ct + 1);
+}
+
+function summarizeResponse(resp, summary) {
+  if (resp.errors) {
+    throw new Error(JSON.stringify(resp.errors));
+  }
+  let nodes = resp.data.organization.projectV2.items.nodes;
+  for (const element of nodes) {
+    if (element.status?.status === DONE) {
+      summary.countDone = summary.countDone + 1;
+      summary.sumHoursDone =
+        summary.sumHoursDone + parseInt(element.hours?.value ?? 0);
+    } else {
+      summary.countNotDone = summary.countNotDone + 1;
+      summary.sumHoursNotDone =
+        summary.sumHoursNotDone + parseInt(element.hours?.value ?? 0);
+    }
+  }
+  return summary;
+}
+
+function sumProjectHoursByStatus(github_token, variables, summary) {
+  const getStatusAndHoursQuery = `
+  query GetStatusAndHours($orgName: String!, $projectNumber: Int!, $after: String, $pages: Int!) {
+    organization(login: $orgName){
+      projectV2(number: $projectNumber){
+        items(first: $pages, after: $after){
+          pageInfo{ hasNextPage endCursor }
+          nodes{
+              hours: fieldValueByName(name: "Hours") {
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  value: name
+                }
+              }
+              status: fieldValueByName(name: "Status") {
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  status: name
+                }
+              }
+          }
+        }
+      }
+    }
+  }
+`;
+
+  const response = UrlFetchApp.fetch("https://api.github.com/graphql", {
+    method: "POST",
+    contentType: 'application/json',
+    headers: {
+      'User-Agent': 'Github Hours Summarize',
+      'Authorization': `Bearer ${github_token}`,
+    },
+    payload: JSON.stringify({
+      query: getStatusAndHoursQuery,
+      variables: variables
+    }),
+  });
+
+  const resp = JSON.parse(response.getContentText());
+  // summarize the response right now so to not save too much response in memory
+  summary = summarizeResponse(resp, summary)
+
+  const pageInfo = resp.data.organization.projectV2.items.pageInfo;
+  if (resp.data && pageInfo.hasNextPage) {
+    variables.after = pageInfo.endCursor;
+    return sumProjectHoursByStatus(variables, summary);
+  }
+
+  return summary;
+}
